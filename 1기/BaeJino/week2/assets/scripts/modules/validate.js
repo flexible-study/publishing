24e89e7e141123676545a95dd25d788bd36e0998
Sieun-Lim <97720335+Sieun-Lim@users.noreply.github.com>
2022-11-17 15:32:05 +0900

    Create Folder

--- 1기/BaeJino/week2/assets/scripts/modules/validate.js
+++
@@ -1,5 +0,0 @@
-export function validate($form) {
-  $form.classList.add('validation-detected')
-  const $inputs = Array.from($form.querySelectorAll('[required]'))
-  return $inputs.map($input => $input.checkValidity()).every(Boolean)
-}
